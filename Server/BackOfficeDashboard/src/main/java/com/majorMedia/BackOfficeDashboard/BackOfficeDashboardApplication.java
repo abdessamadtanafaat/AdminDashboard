a49fb86c4b6ef6940494ee7733a51ad51b8ec4a9
Abdessamad <tanafaat.wac.49@gmail.com>
2024-02-29 18:13:15 +0100

    first commit

---
+++ Server/BackOfficeDashboard/src/main/java/com/majorMedia/BackOfficeDashboard/BackOfficeDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.majorMedia.BackOfficeDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackOfficeDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackOfficeDashboardApplication.class, args);
+	}
+
+}
