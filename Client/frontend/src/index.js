668bee967af202df69e62a19769f7f75c1df5ce0
iliasroc21 <ilyassrouchdi7@gmail.com>
2024-03-04 22:32:30 +0000

    Adding the notifications react-toastify

--- Client/frontend/src/index.js
+++ Client/frontend/src/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'react-toastify/dist/ReactToastify.css';
 import { Provider } from 'react-redux';
 import {store} from './app/store'
+import {ToastContainer} from 'react-toastify'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
     <App />
-
+    <ToastContainer position ="top-center" theme="colored"/>
     </Provider>
-    
     
   </React.StrictMode>
 );
