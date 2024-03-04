c154a74dcfeadad30dd38bf38696e1af7554de2f
iliasroc21 <ilyassrouchdi7@gmail.com>
2024-03-04 22:38:01 +0000

    ForgotPassword Link Added + action logic

--- Client/frontend/src/pages/Login.js
+++ Client/frontend/src/pages/Login.js
@@ -1,7 +1,23 @@
 import { FormInput ,SubmitBtn} from "../components"
-import {Form} from 'react-router-dom'
+import {Form ,redirect , Link} from 'react-router-dom'
+import {customFetch} from '../utils'
+import {toast} from 'react-toastify'
+export const action =(store)=>
+    async({request})=>{
+          const formData = await request.formData();
+          const data = Object.fromEntries(formData);
+          try{
+            const response = await customFetch.post('/api/v1/auth/authenticate' ,data);
+            return redirect("/")
+            
+          }
+          catch(err){
+            console.log(err);
+            toast.success('Item added to cart');
+          }
+          
+          
 
-export const action =(state)=>async({request})=>{
     
 }
 
@@ -10,11 +26,21 @@ const Login = () => {
     <section className='h-screen grid place-items-center'>
         <Form method="POST" className='card w-96 p-8 bg-base-100 shadow-lg flex bg--400 flex-col gap-y-4'>
             <h4 className='text-center text-3xl font-bold'>Login</h4>
-            <FormInput type="text" label="Email" name="Email" placeholder="email@email.com"/>
+            <FormInput type="text" label="Email" name="email" placeholder="email@email.com"/>
             <FormInput type="password" label="Password" name="password" placeholder="secret"/>
+            <p className='text-center font-bold'>
+              Forgot Password ?{' '}
+               <Link
+                 to='/forgotPassword'
+                 className='ml-2 font-semibold  link link-hover link-primary capitalize'
+              >
+                 Reset your Password  
+             </Link>
+        </p>
             <div className='mt-4'>
                <SubmitBtn text='Submit' />
             </div>
+
 
         </Form>
     </section>
